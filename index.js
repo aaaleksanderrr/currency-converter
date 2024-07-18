79ec8bd7c2852e71797c0253d81366c2172f3da6
Aleksander Wypych <aleksander.wypych@gmail.com>
2024-07-18 22:33:35 +0200

    Created a basic currency converter

---
+++ index.js
@@ -0,0 +1,34 @@
+const inputValue = document.getElementById("inputValue");
+const selectCurrency = document.getElementById("selectCurrency");
+const optEUR = document.getElementById("optEUR");
+const optUSD = document.getElementById("optUSD");
+const optCHF = document.getElementById("optCHF");
+const buttonSubmit = document.getElementById("buttonSubmit");
+const pResult = document.getElementById("pResult");
+
+const getCurrency = () => selectCurrency.value;
+const getAmount = () => inputValue.value;
+
+selectCurrency.addEventListener("change", getCurrency);
+inputValue.addEventListener("change", getAmount);
+
+const getCurrencyValue = async () => {
+  try {
+    pResult.textContent = "Loading data, please wait";
+
+    const res = await fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${getCurrency()}/`);
+    const data = await res.json();
+    const rates = data?.rates[0]?.mid;
+
+    if (rates) {
+      pResult.textContent = rates * getAmount();
+    } else {
+      pResult.textContent = "Cannot find specified data";
+    }
+  } catch (err) {
+    console.error("Error: " + err);
+    pResult.textContent = "Error occured, please try again later";
+  }
+};
+
+buttonSubmit.addEventListener("click", getCurrencyValue);
